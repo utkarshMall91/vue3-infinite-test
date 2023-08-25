b9410ef8eba64cb16e2401654110d2a196e22b63
Utkarsh Joshi <utkarsh@mall91.com>
2023-08-25 15:38:47 +0530

    inital commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import InfiniteLoading from "v3-infinite-loading";
+import "v3-infinite-loading/lib/style.css"; //required if you're not going to override default slots
 
-createApp(App).mount('#app')
+const app = createApp(App).mount("#app");
+app.component("infinite-loading", InfiniteLoading);
